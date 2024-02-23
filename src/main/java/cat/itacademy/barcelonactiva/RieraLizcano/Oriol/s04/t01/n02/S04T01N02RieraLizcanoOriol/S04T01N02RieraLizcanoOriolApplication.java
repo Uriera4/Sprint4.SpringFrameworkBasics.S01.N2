18af1a752b3635a8c6cd601c5f81a31a61a70a5f
urier <orarpione@gmail.com>
2024-02-23 16:10:26 +0100

    "nivell2"

---
+++ src/main/java/cat/itacademy/barcelonactiva/RieraLizcano/Oriol/s04/t01/n02/S04T01N02RieraLizcanoOriol/S04T01N02RieraLizcanoOriolApplication.java
@@ -0,0 +1,11 @@
+package cat.itacademy.barcelonactiva.RieraLizcano.Oriol.s04.t01.n02.S04T01N02RieraLizcanoOriol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N02RieraLizcanoOriolApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N02RieraLizcanoOriolApplication.class, args);
+	}
+}
